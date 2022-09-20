0c4898681c9a4b02b68d54d719cce02d5c2f03b0
Alhay Alhamely <alhayalhamely@Alhays-MacBook-Air.local>
2022-09-20 19:26:31 +0400

    continue parsing

---
+++ PhiloFunc/Philo.h
@@ -0,0 +1,24 @@
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <math.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <stdlib.h>
+
+
+void	check_sum(unsigned long sum, int sign, const char *str);
+
+int     ft_atoi(const char *str);
+
+void	ft_putchar_fd(int c, int fd);
+
+void    ft_putstr_fd(char *str, int fd);
+
+void	ft_putendl_fd(char *str, int fd);
+
+
+
+#endif
--- PhiloFunc/philo1.c
+++ PhiloFunc/philo1.c
@@ -1,2 +1,10 @@
 //first  solution for Philo
+#include "Philo.h"
 
+int main(int arc, char **arv)
+{
+    while (arc > 1)
+    {
+        while(argv)
+    }
+}
